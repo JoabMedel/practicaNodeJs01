021a561a42ae4af8cb854f4d7fba62e075d86423
JoabMedel <65578244+JoabMedel@users.noreply.github.com>
2021-01-06 19:14:36 -0600

    servidor web sin morgan

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 let http = require("http");
-let morgan = require("morgan");
-let logger = morgan('dev');
 
 http.createServer((request,response) => {
     //dentro se escribe toda la logica al momento de hacer una peticion
-    logger(request, response, (err) => {
         if (err) return done(err)
-        
         if(request.url === "/contacto"){
             response.setHeader("Content-Type", "text/html; charset=utf-8");
             response.write("<h1>Contacto</h1>");
@@ -21,5 +17,4 @@ http.createServer((request,response) => {
             response.end();
         }
         // respond to request
-      })
 }).listen(8080)
